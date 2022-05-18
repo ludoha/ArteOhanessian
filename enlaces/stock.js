129b496e460fd83278361f8f06f33d42cc780d46
ludoha <ludmilaohanessian@hotmail.com>
2022-05-17 23:11:33 -0300

    Modificaciones

---
+++ enlaces/stock.js
@@ -0,0 +1,12 @@
+let stockProductos = [
+    {id: 1, nombre: "Condor Andino", tecnica: "Microfibra sobre papel", tipo: "Dibujo", cantidad: 1,  precio: 7000, img: "../img3/condorandino.jpg"},
+    {id: 2, nombre: "YUYA", tecnica: "Acuarela sobre papel", tipo: "Acuarela", cantidad: 1,  precio: 8000, img: "../img3/acuarelayuya.jpg"},
+    {id: 3, nombre: "Torso", tecnica: "Yeso", tipo: "Escultura", cantidad: 1, precio: 50000, img: "../img3/torsoyeso.jpg"},
+    {id: 4, nombre: "Mujer Felina", tecnica: "Microfibra sobre papel", tipo: "Dibujo", cantidad: 1, precio: 7000, img: "../img3/mujerfelina.jpg"},
+    {id: 5, nombre: "Dani", tecnica: "Oleo sobre tela", tipo: "Pintura", cantidad: 1, precio: 50000, img: "../img3/oleo.jpg"},
+    {id: 6, nombre: "Terracota", tecnica: "Modelado a mano y horneado", tipo: "Escultura", cantidad: 1, precio: 50000, img: "../img3/terracota.jpg"},
+    {id: 7, nombre: "Esteros del Iberá", tecnica: "Microfibra sobre papel", tipo: "Dibujo", cantidad: 1, precio: 7000, img: "../img3/ibera.jpg"},
+    {id: 8, nombre: "Campiña Francesa", tecnica: "Oleo sobre tela", tipo: "Pintura", cantidad: 1, precio: 60000, img: "../img3/campiña.jpg"},
+    {id: 9, nombre: "Caos", tecnica: "Escultura de ensamblaje", tipo: "Escultura", cantidad: 1, precio: 40000, img: "../img3/ensamble.jpg"},
+]
+console.log(...stockProductos);
